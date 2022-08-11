9258e9499eb29f6d0d0a46fdd809b743a837c952
CR1-05\Student <x05290310@gmail.com>
2022-08-11 15:34:25 +0800

    圖表修改

--- admin/Controllers/HomeController.cs
+++ admin/Controllers/HomeController.cs
@@ -38,6 +38,9 @@ namespace admin.Controllers
             List<int> itemsList = new List<int>();
             List<int> 銷售數量 = new List<int>();
             List<int> 觀看次數 = new List<int>();
+            List<int> 會員成長數 = new List<int>();
+            List<int> 金額總和 = new List<int>();
+
             //依照需求撈資料
             var mydata = _context.Items.
                 GroupBy(m => m.賣方id).
@@ -53,16 +56,42 @@ namespace admin.Controllers
             //    ()
             //    }).ToList();
 
+            DateTime date6 = new DateTime(2022, 6, 1, 0, 0, 0);
+            DateTime date7 = new DateTime(2022, 7, 1, 0, 0, 0);
+            DateTime date8 = new DateTime(2022, 8, 1, 12, 0, 0);
+            DateTime date9 = new DateTime(2022, 9, 1, 12, 0, 0);
+
             var 銷量 = _context.OrderOrderings.GroupBy(m => m.子項名稱快照).OrderByDescending(x => x.Count()).
                 Select(x => new { label = x.Key, data = x.Count() }).ToList()
                 ;
-            //var 瀏覽次數 = _context.Items.Select(p => p.瀏覽次數).OrderByDescending(x => x).ToList();
-         
 
 
-                //var 金額 = from s in _context.OrderInfos
-                //          where s.付款金額.CompareTo("2017/04/12") >= 0 && s.創建時間.CompareTo("2017/04/15") <= 0
-                //          select s;
+            var 瀏覽次數 = _context.Items.Select(p => p.瀏覽次數).OrderByDescending(x => x).ToList();
+            var  七月金額 =( from num  in  _context.OrderInfos
+                      where num.創建時間 >date7 && num.創建時間<date8                    
+                      select num.付款金額).Sum();
+
+            var 八月金額 = (from num in _context.OrderInfos
+                        where num.創建時間 > date8 && num.創建時間 < date9
+
+                        select num.付款金額).Sum();
+
+
+            var 六月人數 = (from number in _context.Users
+                        where number.註冊時間 > date6 && number.註冊時間 < date7
+
+                        select number.會員帳號).Count();
+            var 七月人數 = (from number in _context.Users
+                       where number.註冊時間 > date6 && number.註冊時間 < date8
+
+                       select number.會員帳號).Count();
+            var 八月人數 = (from number in _context.Users
+                        where number.註冊時間 > date6 && number.註冊時間 < date9
+
+                        select number.會員帳號).Count();
+            //var 金額 = from s in _context.OrderInfos
+            //          where s.付款金額.CompareTo("2017/04/12") >= 0 && s.創建時間.CompareTo("2017/04/15") <= 0
+            //          select s;
 
             //把資料存成List
             foreach (var item in mydata)
@@ -70,26 +99,41 @@ namespace admin.Controllers
                 itemsList.Add(item.data);
             }
 
-            foreach (var item in 銷量)
+
+            int sales = 0;
+                foreach (var item in 銷量)
+                {
+                if (sales >= 4) break;
+                    銷售數量.Add(item.data);
+                sales ++;
+
+                }
+            
+       
+
+            foreach (var item in 瀏覽次數)
             {
-                銷售數量.Add(item.data);
+                觀看次數.Add(item.Value);
 
             }
 
-            //foreach (var item in 瀏覽次數)
-            //{
-            //    觀看次數.Add(item.Value);
-
-            //}
-
             觀看次數.Sort();
             觀看次數.Reverse();
+
+            會員成長數.Add(六月人數);
+            會員成長數.Add(七月人數);
+            會員成長數.Add(八月人數);
+
+            金額總和.Add(七月金額.Value);
+            金額總和.Add(八月金額.Value);
 
 
             //序列化
             ViewBag.itemsData = JsonSerializer.Serialize(itemsList);
             ViewBag.銷售數量 = JsonSerializer.Serialize(銷售數量);
-            //ViewBag.瀏覽次數 = JsonSerializer.Serialize(觀看次數);
+            ViewBag.瀏覽次數 = JsonSerializer.Serialize(觀看次數);
+            ViewBag.銷售金額 = JsonSerializer.Serialize(金額總和);
+            ViewBag.成長人數 = JsonSerializer.Serialize(會員成長數);
             return View();
         }
 
